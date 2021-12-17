45dd06dc4c13b4e52fd3d9292f1816575d16310b
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-12-17 12:36:32 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+var a1;
+var a2, a3;
+var a_1, a$2;
+var ab;
+/* */
+// Data Types
+// number(int,long,double,float,short),string (""/''), boolean(true/false),
+// any(number,string,boolean),void (netural=> it does not point any thing)
+// misec. => null & undefined => we can use as value 
+var a = 12; //forward decl. & defination
+console.log('Value of a is ' + a);
