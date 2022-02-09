28a20e4f58fd2f37a4c8b4911ef2cce822dae9e8
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-02-09 16:46:33 +0530

    Day24

---
+++ Angular/Day24/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ParticlesModule } from 'angular-particle'; 
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ParticlesModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
