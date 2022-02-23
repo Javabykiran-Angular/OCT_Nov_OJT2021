9b2aee59153731f728c9d67658d90f2bd0f6fd05
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-02-23 16:01:13 +0530

    Day31

---
+++ Angular/Day31/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ParticlesModule } from 'angular-particle'; 
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';  
+import { NgxPaginationModule } from 'ngx-pagination';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+import { MatConfirmDialogComponent } from './shared/mat-confirm-dialog/mat-confirm-dialog.component';
+import { ToastrModule } from 'ngx-toastr';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    EmployeeDetailsComponent,
+    AddemployeeComponent,
+    NotfoundComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ParticlesModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ToastrModule.forRoot({
+      timeOut:5000,
+      positionClass:'toast-top-right',
+      preventDuplicates:true
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents:[MatConfirmDialogComponent]
+})
+export class AppModule { }
